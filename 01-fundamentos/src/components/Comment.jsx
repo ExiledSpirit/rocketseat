0b7ce30a810cb480336aa918a3f8c741bc4f83b6
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-23 12:40:30 -0300

    Componentizando avatar

--- 01-fundamentos/src/components/Comment.jsx
+++ 01-fundamentos/src/components/Comment.jsx
@@ -1,16 +1,17 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src='https://github.com/ExiledSpirit.png' />
+      <Avatar hasBorder={true} src='https://github.com/ExiledSpirit.png'/>
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Gabriel Gonçalves</strong>
               <time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:30'>Cercade 1h atrás</time>
             </div>
 
