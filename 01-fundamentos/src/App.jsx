3a232d8e463a1713acefbf7bbef4018bbb98bc69
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-23 00:14:02 -0300

    Estrutura de HTML da página

--- 01-fundamentos/src/App.jsx
+++ 01-fundamentos/src/App.jsx
@@ -1,24 +1,29 @@
-import { Post } from './Post'
-import { Header } from './components/Header'
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+
+import styles from './App.module.css';
 
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
-      />
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
-      />
-      <Post
-        author="Gabriel"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Gabriel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
+          />
+          <Post
+            author="Gabriel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
