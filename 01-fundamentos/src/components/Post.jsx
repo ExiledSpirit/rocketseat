1da8ab21d09ea6537e70edce8c7dcd076773945e
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-23 17:29:12 -0300

    Adicionando propriedade key e porquê de não utilizar o índice do array como key

--- 01-fundamentos/src/components/Post.jsx
+++ 01-fundamentos/src/components/Post.jsx
@@ -58,10 +58,10 @@ export function Post({author, content, publishedAt}) {
         {
           content.map(line => {
             if (line.type === 'paragraph') {
-              return <p>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             }
             if (line.type === 'link') {
-              return <p><a href='#'>{line.content}</a></p>
+              return <p key={line.content}><a href='#'>{line.content}</a></p>
             }
           })
         }
@@ -84,7 +84,7 @@ export function Post({author, content, publishedAt}) {
       <div className={styles.commentList}>
         {
           comments.map(comment => {
-            return <Comment content={comment} />
+            return <Comment key={comment} content={comment} />
           })
         }
       </div>
