6b66302f085958b5a8f47e4236242a7fd224aa6e
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-23 12:22:01 -0300

    Adicionando listagem de comentários

---
+++ 01-fundamentos/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/ExiledSpirit.png' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:30'>Cercade 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, Diego, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
