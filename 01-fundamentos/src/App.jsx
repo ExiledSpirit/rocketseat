9826fbec561d80ed8fbe340864edb2c8b5a7cabe
ExiledSpirit <gab.gabriel.1002@outlook.com>
2022-12-23 14:30:47 -0300

    Adicionando propriedades no componente de posts

--- 01-fundamentos/src/App.jsx
+++ 01-fundamentos/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare', }, 
+    ],
+    publishedAt: new Date('2022-12-20 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+      { type: 'link', content: 'jane.design/doctorcare', }, 
+    ],
+    publishedAt: new Date('2022-12-12 20:00:00'),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,14 +45,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Gabriel"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
-          />
-          <Post
-            author="Gabriel"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor ex veniam beatae labore ullam excepturi asperiores illo iusto similique laboriosam praesentium soluta distinctio voluptate sequi, culpa animi eum aliquam!"
-          />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
