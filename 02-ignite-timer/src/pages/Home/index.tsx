d4d2e9b2bbc7083d8bc615e9cde21de29bef9685
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2023-01-11 13:27:21 -0300

    Adicionando countdown

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import {
   CountdownContainer,
   FormContainer,
@@ -6,6 +6,7 @@ import {
   MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from './styles'
 
@@ -14,7 +15,8 @@ import { useForm } from 'react-hook-form';
 import {zodResolver} from '@hookform/resolvers/zod';
 
 import * as zod from 'zod';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {differenceInSeconds } from 'date-fns';
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -25,6 +27,8 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
 }
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
@@ -56,6 +60,25 @@ export function Home() {
   const task = watch('task');
   const isSubmitDisabled = !task;
  
+  useEffect(() => {
+    if (activeCycle) document.title = `${minutes}:${seconds}`;
+  }, [minutes, seconds, activeCycle]);
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate));
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval);
+    }
+
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
     
@@ -63,12 +86,28 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles(updatedTasks => [...updatedTasks, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
 
     reset();
+  }
+
+  function handleInterruptCycle() {
+    setCycles(cycles =>
+      cycles.map(cycle => {
+        if (cycle.id === activeCycleId) {
+          return {...cycle, interruptedDate: new Date()};
+        } else {
+          return cycle;
+        }
+      }),
+    );
+
+    setActiveCycleId(null);
   }
 
   return (
@@ -80,6 +119,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
           <datalist id="task-suggestions">
@@ -96,6 +136,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', {valueAsNumber: true})}
           />
 
@@ -110,10 +151,23 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {
+          activeCycle
+          ?
+          (
+            <StopCountdownButton onClick={handleInterruptCycle} type="button">
+              <HandPalm size={24} />
+              Interromper
+            </StopCountdownButton>
+          )
+          :
+          (
+            <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+              <Play size={24} />
+              Começar
+            </StartCountdownButton>
+          )
+        }
       </form>
     </HomeContainer>
   )
--- 02-ignite-timer/src/pages/Home/styles.ts
+++ 02-ignite-timer/src/pages/Home/styles.ts
@@ -87,7 +87,8 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
+  color: ${(props) => props.theme['gray-100']};
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -102,15 +103,25 @@ export const StartCountdownButton = styled.button`
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
